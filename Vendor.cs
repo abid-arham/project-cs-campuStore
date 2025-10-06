e3cc3eb56e83f360e59b62ac6a22eb18edc4c9b7
abid-arham <mix.arham@gmail.com>
2025-10-07 02:35:34 +0600

    Vendor created

---
+++ Vendor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CampuStore
+{
+    public partial class Vendor : Form
+    {
+        public Vendor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDashboard_MouseEnter(object sender, EventArgs e)
+        {
+            btnDashboard.BackColor = Color.Blue;
+        }
+
+        private void btnDashboard_MouseLeave(object sender, EventArgs e)
+        {
+            btnDashboard.BackColor = SystemColors.ControlLight;
+        }
+    }
+}
