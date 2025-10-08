6725cc222395047ec821cdd9922aacb513eb8b15
sumiya-rahman-hadir <srahmanhadir@gmail.com>
2025-10-08 12:44:01 +0600

    Vendor updated

---
+++ NewProduct.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace CampuStore
+{
+    partial class NewProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.chkBooks = new System.Windows.Forms.CheckBox();
+            this.chkElectronics = new System.Windows.Forms.CheckBox();
+            this.chkStationary = new System.Windows.Forms.CheckBox();
+            this.txtBrand = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblcategory = new System.Windows.Forms.Label();
+            this.lblBrand = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.61996F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.38004F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(699, 395);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtQuantity);
+            this.panel2.Controls.Add(this.txtProductID);
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.txtPrice);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.txtBrand);
+            this.panel2.Controls.Add(this.txtProductName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(189, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(507, 389);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(33, 48);
+            this.txtProductID.Multiline = true;
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.ReadOnly = true;
+            this.txtProductID.Size = new System.Drawing.Size(412, 33);
+            this.txtProductID.TabIndex = 11;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(195, 344);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 38);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(33, 256);
+            this.txtPrice.Multiline = true;
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(412, 33);
+            this.txtPrice.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.chkBooks);
+            this.panel3.Controls.Add(this.chkElectronics);
+            this.panel3.Controls.Add(this.chkStationary);
+            this.panel3.Location = new System.Drawing.Point(33, 204);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(412, 33);
+            this.panel3.TabIndex = 8;
+            // 
+            // chkBooks
+            // 
+            this.chkBooks.AutoSize = true;
+            this.chkBooks.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBooks.Location = new System.Drawing.Point(3, 3);
+            this.chkBooks.Name = "chkBooks";
+            this.chkBooks.Size = new System.Drawing.Size(80, 27);
+            this.chkBooks.TabIndex = 5;
+            this.chkBooks.Text = "Books";
+            this.chkBooks.UseVisualStyleBackColor = true;
+            // 
+            // chkElectronics
+            // 
+            this.chkElectronics.AutoSize = true;
+            this.chkElectronics.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkElectronics.Location = new System.Drawing.Point(237, 3);
+            this.chkElectronics.Name = "chkElectronics";
+            this.chkElectronics.Size = new System.Drawing.Size(117, 27);
+            this.chkElectronics.TabIndex = 7;
+            this.chkElectronics.Text = "Electronics";
+            this.chkElectronics.UseVisualStyleBackColor = true;
+            // 
+            // chkStationary
+            // 
+            this.chkStationary.AutoSize = true;
+            this.chkStationary.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkStationary.Location = new System.Drawing.Point(101, 3);
+            this.chkStationary.Name = "chkStationary";
+            this.chkStationary.Size = new System.Drawing.Size(116, 27);
+            this.chkStationary.TabIndex = 6;
+            this.chkStationary.Text = "Stationary";
+            this.chkStationary.UseVisualStyleBackColor = true;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.Location = new System.Drawing.Point(33, 152);
+            this.txtBrand.Multiline = true;
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(412, 33);
+            this.txtBrand.TabIndex = 1;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(33, 100);
+            this.txtProductName.Multiline = true;
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(412, 33);
+            this.txtProductName.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblProductID);
+            this.panel1.Controls.Add(this.lblPrice);
+            this.panel1.Controls.Add(this.lblQuantity);
+            this.panel1.Controls.Add(this.lblcategory);
+            this.panel1.Controls.Add(this.lblBrand);
+            this.panel1.Controls.Add(this.lblProductName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 389);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductID.Location = new System.Drawing.Point(48, 46);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(96, 23);
+            this.lblProductID.TabIndex = 6;
+            this.lblProductID.Text = "Product ID";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(95, 258);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(49, 23);
+            this.lblPrice.TabIndex = 5;
+            this.lblPrice.Text = "Price";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(66, 311);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(80, 23);
+            this.lblQuantity.TabIndex = 4;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // lblcategory
+            // 
+            this.lblcategory.AutoSize = true;
+            this.lblcategory.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcategory.Location = new System.Drawing.Point(60, 205);
+            this.lblcategory.Name = "lblcategory";
+            this.lblcategory.Size = new System.Drawing.Size(84, 23);
+            this.lblcategory.TabIndex = 3;
+            this.lblcategory.Text = "Category";
+            // 
+            // lblBrand
+            // 
+            this.lblBrand.AutoSize = true;
+            this.lblBrand.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBrand.Location = new System.Drawing.Point(86, 152);
+            this.lblBrand.Name = "lblBrand";
+            this.lblBrand.Size = new System.Drawing.Size(58, 23);
+            this.lblBrand.TabIndex = 1;
+            this.lblBrand.Text = "Brand";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.Location = new System.Drawing.Point(19, 99);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(125, 23);
+            this.lblProductName.TabIndex = 0;
+            this.lblProductName.Text = "Product Name";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(33, 308);
+            this.txtQuantity.Multiline = true;
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(412, 33);
+            this.txtQuantity.TabIndex = 12;
+            // 
+            // NewProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 395);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "NewProduct";
+            this.Text = "NewProduct";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.TextBox txtBrand;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblcategory;
+        private System.Windows.Forms.Label lblBrand;
+        private System.Windows.Forms.CheckBox chkStationary;
+        private System.Windows.Forms.CheckBox chkBooks;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.CheckBox chkElectronics;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtProductID;
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.TextBox txtQuantity;
+    }
+}
--- Vendor.cs
+++ Vendor.cs
@@ -26,5 +26,54 @@ namespace CampuStore
         {
             btnDashboard.BackColor = SystemColors.ControlLight;
         }
+
+        private void btnNewProduct_Click(object sender, EventArgs e)
+        {
+            this.pnlMain.Controls.Clear();
+            NewProduct form = new NewProduct();
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.AutoScroll = true;
+            form.Dock = DockStyle.Fill;
+            this.pnlMain.Controls.Add(form);
+            form.Show();
+        }
+
+        private void btnVendorInfo_Click(object sender, EventArgs e)
+        {
+            this.pnlMain.Controls.Clear();
+            VendorInfo form = new VendorInfo();
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.AutoScroll = true;
+            form.Dock = DockStyle.Fill;
+            this.pnlMain.Controls.Add(form);
+            form.Show();
+
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            this.pnlMain.Controls.Clear();
+            Dashboard form = new Dashboard();
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.AutoScroll = true;
+            form.Dock = DockStyle.Fill;
+            this.pnlMain.Controls.Add(form);
+            form.Show();
+        }
+
+        private void Vendor_Load(object sender, EventArgs e)
+        {
+            this.pnlMain.Controls.Clear();
+            Dashboard form = new Dashboard();
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.AutoScroll = true;
+            form.Dock = DockStyle.Fill;
+            this.pnlMain.Controls.Add(form);
+            form.Show();
+        }
     }
 }
